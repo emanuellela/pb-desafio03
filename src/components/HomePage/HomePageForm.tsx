a2304e7b4dec7c67e4586ed7793ae840905d8eab
emanuellela <emanuellela@hotmail.com>
2023-08-15 08:30:08 -0300

    estilo na home page

--- src/components/HomePage/HomePageForm.tsx
+++ src/components/HomePage/HomePageForm.tsx
@@ -16,23 +16,17 @@ const HomePageForm: React.FC<HomePageFormProps> = () => {
 
       <div className="homep-container">
         <div className="form-group-text">
-            {[
-                { text: 'Premium', className: 'tx1' },
-                { text: 'quality', className: 'tx2' },
-                { text: 'Food for your', className: 'tx3' },
-                { text: 'healthy', className: 'tx4' },
-                { text: '& Daily Life', className: 'tx5' }
-            ].map((item, index) => (
-                <h1 key={index} className={item.className}>
-                {item.text}
-                </h1>
-            ))}
-          </div>
-          <p className='ptext'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut<br />
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco<br />
-            laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
+          <h1 className="tx1">Premium </h1>
+          <h1 className="tx2">quality</h1><br />
+          <h1 className="tx3">Food for your</h1>
+          <h1 className="tx4">healthy</h1><br />
+          <h1 className="tx5">& Daily Life</h1>
+        </div>
+        <p className='ptext'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut<br />
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco<br />
+          laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
       </div>
       <Footer />
     </div>
