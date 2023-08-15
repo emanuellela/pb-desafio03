5f38a5cc61e7da1b949681215e20e97a6155f57c
emanuellela <emanuellela@hotmail.com>
2023-08-15 07:37:06 -0300

    arrumando logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import ReactDOM from 'react-dom';
 import LoginForm from './components/Login/LoginForm';
 import RegisterForm from './components/Register/RegisterForm';
 import HomePageForm from './components/HomePage/HomePageForm';
+import Logo from './components/Logo/Logo';
 
 function App() {
   return (
     <div className="App">
+      {/* <Logo /> */}
       {/* <LoginForm /> */}
       {/* <RegisterForm /> */}
       <HomePageForm />
-      </div>
+    </div>
   );
 }
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
